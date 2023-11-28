e4684d28e0cef75d028ba0ddff1f2ef592025309
unknown <akshatupadhyay2@gmail.com>
2023-11-28 13:23:07 +0530

    added jwt authentication

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -1,13 +1,20 @@
 import { UserModel } from "./user.model.js";
+import jwt from "jsonwebtoken";
 
 export class UserController {
   signIn(req, res) {
-    const result = UserModel.signIn(req.body.email,req.body.password);
-    if(!result){
-       return res.status(400).send('Incorrect credentials')
-    }
-    else{
-        return res.send("Login successful");
+    const result = UserModel.signIn(req.body.email, req.body.password);
+    if (!result) {
+      return res.status(400).send("Incorrect credentials");
+    } else {
+      // 1. Create JWT
+      const token = jwt.sign(
+        { userID: result.id, email: result.email },
+        "7H9oNtw7VmBrhAgUUxPlNk2wEaHjDvZr",
+        { expiresIn: "1h" }
+      );
+      // 2. Send token
+      return res.status(200).send(token);
     }
   }
 
---
+++ src/middlewares/jwt.middleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const jwtAuth = (req, res, next) => {
+  // 1. Read token
+  const token = req.headers["authorization"];
+  //2. If no token return error
+  if (!token) {
+    res.status(401).send("Unauthorized");
+  }
+  //3. Check if token is valid
+  try {
+    const payload = jwt.verify(token, "7H9oNtw7VmBrhAgUUxPlNk2wEaHjDvZr");
+  } catch (err) {
+    // 4. return error
+    res.status(401).send("Unauthorized");
+  }
+  //. 5. Call next middleware
+  next();
+};
+
+export default jwtAuth;
