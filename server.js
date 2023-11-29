5c508c31c38579a9ff1c9bc617088c360137a8f0
unknown <akshatupadhyay2@gmail.com>
2023-11-29 18:25:31 +0530

    added api documentation with swagger

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import swagger from 'swagger-ui-express';
 import bodyParser from 'body-parser';
 import productRouter from './src/features/product/product.routes.js';
 import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
 import userRouter from './src/features/user/user.routes.js';
 import cartRouter from './src/features/cartItem/cartItems.routes.js';
+import apiDocs from './swagger.json' assert {type:'json'};
 
 const server = express();
 server.use(bodyParser.json());
+server.use('/api-docs/',swagger.serve,swagger.setup(apiDocs));
 server.use('/api/products',jwtAuth, productRouter);
 server.use('/api/cartitems',jwtAuth, cartRouter);
 server.use('/api/users',userRouter);
