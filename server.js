07928ee9b58d6d3e4da1c9e1ad9d05fd78c8e2f7
unknown <akshatupadhyay2@gmail.com>
2023-11-30 16:19:44 +0530

    alternative method, removed cors error using cors library

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import swagger from "swagger-ui-express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import productRouter from "./src/features/product/product.routes.js";
 import basicAuthorizer from "./src/middlewares/basicAuth.middleware.js";
 import jwtAuth from "./src/middlewares/jwt.middleware.js";
@@ -9,18 +10,22 @@ import cartRouter from "./src/features/cartItem/cartItems.routes.js";
 import apiDocs from "./swagger.json" assert { type: "json" };
 
 const server = express();
-
 // CORS policy configurartion
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5500"); // if you want to allow access to all the clients you can use '*' instead of specified port
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  // return OK for preflight request
-  if(req.method=="OPTIONS"){
-    return res.sendStatus(200);
-  }
-  next();
-});
+
+let corsOptions = {
+    origin:"http://localhost:5500"
+}
+server.use(cors(corsOptions));
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:5500"); // if you want to allow access to all the clients you can use '*' instead of specified port
+//   res.header("Access-Control-Allow-Headers", "*");
+//   res.header("Access-Control-Allow-Methods", "*");
+//   // return OK for preflight request
+//   if(req.method=="OPTIONS"){
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 server.use(bodyParser.json());
 server.use("/api-docs/", swagger.serve, swagger.setup(apiDocs));
 server.use("/api/products", jwtAuth, productRouter);
